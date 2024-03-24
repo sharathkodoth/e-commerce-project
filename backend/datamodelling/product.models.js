15df9db429a57c09399e81376158956bda33b9ba
sharath chandran <sharathchandrank91@gmail.com>
2024-03-24 20:00:19 +0530

    changes

--- backend/datamodelling/product.models.js
+++ backend/datamodelling/product.models.js
@@ -1,33 +1,39 @@
 import mongoose from "mongoose";
 
-const productSchema = mongoose.Schema({
+const productSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     description: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     productImage: {
-        type: String, // string bc img are usually stored in cloud buckets and an url of img is returned
-
+      type: String, // string bc img are usually stored in cloud buckets and an url of img is returned
     },
     price: {
-        type: Number,
-        required: true,
-        default: 0
+      type: Number,
+      required: true,
+      default: 0,
     },
     stock: {
-        type: Number,
-        required: true,
-        default: 0
+      type: Number,
+      required: true,
+      default: 0,
     },
     category: {
-        type: mongoose.Schema.Types.ObjectId, //refering category model
-        ref: "Category",
-        required: true
-    }
-}, {timestamps: true})
+      type: mongoose.Schema.Types.ObjectId, //refering category model
+      ref: "Category",
+      required: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
 
-export const Product = mongoose.model("Product", productSchema)
+export const Product = mongoose.model("Product", productSchema);
